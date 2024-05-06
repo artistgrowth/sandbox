b2ad4c70be9c77fbdfa66a14b6de0a86bddfcd6e
Andrew Alizaga <42665392+AndrewAlizaga@users.noreply.github.com>
2024-05-06 01:56:08 -0600

    fixed typo on file name

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
-from . import custome_router
+from . import custom_router
 from . import views
 
-router = custome_router.PatchRouter()
+router = custom_router.PatchRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 router.register(r"questions", views.QuestionViewSet)
@@ -12,6 +12,5 @@ router.register(r"choices", views.ChoiceViewSet)
 # app_name = "api"
 urlpatterns = [
     path("", include(router.urls)),
-    #path("questions/", views.QuestionViewSet.as_view(actions={'get': 'list', 'post': 'create'}), name="api-question"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
 ]
