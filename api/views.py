bd407c2639b6cf673ad59f7510ddd969ce51468e
richard <richard.atuan@gmail.com>
2024-05-14 19:59:03 -0400

    Add patch method into QuestionViewSet

--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User, Group
+from django.shortcuts import get_object_or_404
+
 from rest_framework import permissions
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 
 from polls.models import Question, Choice
 from .serializers import UserSerializer, GroupSerializer, QuestionSerializer, ChoiceSerializer
@@ -32,6 +37,37 @@ class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def patch(self, request: Request, pk=None):
+        """
+        Method to partial update the information of a question
+        
+        Parameters
+        ----------
+        request: rest_framework.request.Request
+            Request received with the information to update
+        
+        pk: Int
+            Primary key or id of the object to update
+        
+        Returns
+        -------
+        rest_framework.response.Response
+            Response with the id, and the data that was updated
+        """
+        if isinstance(request.data, list):
+            ids = [d["url"].split("/")[-2] for d in request.data]
+            ids_updated = []
+            for id, data in zip(ids, request.data):
+                question = get_object_or_404(Question, pk=id)
+                question.question_text = data["question_text"]
+                question.save()
+                ids_updated.append((id, data["question_text"]))
+            return Response(data={"results": ids_updated}, status=status.HTTP_200_OK)
+        else:
+            question = get_object_or_404(Question, pk=pk)
+            question.question_text = data["question_text"]
+            question.save()
+            return Response(data={"results": [(pk, data["question_text"])]}, status=status.HTTP_200_OK)
 
 class ChoiceViewSet(viewsets.ModelViewSet):
     """
